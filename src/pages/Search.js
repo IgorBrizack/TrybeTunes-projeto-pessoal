74892ecd05b4815df8e316881209e92c39b334b1
Igor Brizack <igor_brizack@hotmail.com>
2022-05-18 19:34:06 -0300

    questão 5 concluída

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,13 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    isDisabled: true,
+    search: '',
+  }
+
+  checkSearch = () => {
+    const { search } = this.state;
+    if (search.length >= 1) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  }
+
+  onInputChange = ({ target }) => this.setState({ [target.name]: target.value },
+    this.checkSearch());
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>
-          search
-        </p>
+        <form>
+          <label htmlFor="search">
+            <input
+              name="search"
+              onChange={ this.onInputChange }
+              id="search"
+              data-testid="search-artist-input"
+              type="text"
+            />
+          </label>
+          <button
+            // onClick=''
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
