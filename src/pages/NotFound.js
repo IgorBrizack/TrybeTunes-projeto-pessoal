928d1c7e100f6df65fe5c77da36f6a975635445e
Igor Brizack <igor_brizack@hotmail.com>
2022-06-13 17:57:26 -0300

    page not found css

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,10 +3,10 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        <p>
-          notFound
-        </p>
+      <div data-testid="page-not-found" className="page-not-found">
+        <h1>
+          Ops! Página não encontrada!
+        </h1>
       </div>
     );
   }
